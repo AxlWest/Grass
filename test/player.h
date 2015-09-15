3d1fe64969786cff20f649b538fdb828920430c8
AxlWest <axlwest@hotmail.com>
2015-09-15 14:06:33 +0100

    Player
    the start of the player class

---
+++ test/player.h
@@ -0,0 +1,11 @@
+class Card ;
+class Deck ;
+
+class Player
+{
+	protected :
+		
+		static int MAX_HAND_SIZE = 7 ; //The maximum number of cards in your hand at any time is 7
+		
+		Card* hand[MAX_HAND_SIZE] ; //Stores the cards that are in your hand
+} ;
